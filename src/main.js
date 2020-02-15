0666333e219daaf48347a0c07d6f05ac688a5594
Dusty <dustymccord@gmail.com>
2020-02-15 10:41:14 -0800

    add firebase, firebaseui, and enable firebase google auth login.

--- src/main.js
+++ src/main.js
@@ -1,15 +1,64 @@
-// import { example } from './example';
-import './scss/main.scss';
-import $ from 'jquery';
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import * as firebaseui from "firebaseui";
+
+import "./scss/main.scss";
+import $ from "jquery";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC-YzJrwqc7dqtxy1dGAMvAvymJ-ZF1F3M",
+  authDomain: "karaoke-team.firebaseapp.com",
+  databaseURL: "https://karaoke-team.firebaseio.com",
+  projectId: "karaoke-team",
+  storageBucket: "karaoke-team.appspot.com",
+  messagingSenderId: "34887574579",
+  appId: "1:34887574579:web:3e3d24e14b4ae3830a6f46",
+  measurementId: "G-G0KQY59TBD"
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+  // Initialize the FirebaseUI Widget using Firebase.
+}
+
+// let db = firebase.firestore();
+let ui = new firebaseui.auth.AuthUI(firebase.auth());
 
 $(document).ready(function() {
+  let userID;
+
+  //Login condition
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      userID = firebase.auth().currentUser.uid;
+      $(".login").hide();
+      $(".site").show();
+      console.log(userID);
+    } else {
+      $(".site").hide();
+      ui.start("#firebaseui-auth-container", {
+        signInSuccessUrl: "#",
+        signInOptions: [
+          // List of OAuth providers supported.
+          firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+          firebase.auth.GithubAuthProvider.PROVIDER_ID
+        ],
+        // Terms of service url.
+        tosUrl: "#",
+        // Privacy policy url.
+        privacyPolicyUrl: "#"
+      });
+    }
+  });
+
   //get form submit button
-  $('form').submit((event) => {
+  $("form").submit((event) => {
     event.preventDefault();
-    let input1 = $('#input-1').val();
-    let input2 = $('#input-2').val();
+    let input1 = $("#input-1").val();
+    let input2 = $("#input-2").val();
 
     //print to DOM
-    $('.output').html(input1 + ' ' + input2);
+    $(".output").html(input1 + " " + input2);
   });
 });
--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
     new CleanWebpackPlugin(),
     new Dotenv(),
     new HtmlWebpackPlugin({
-      title: "webpack",
+      title: "karaoke-team",
       template: "./src/index.html",
       inject: "body"
     })
