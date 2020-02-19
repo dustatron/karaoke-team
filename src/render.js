665d91a9e810ba676a31ad0c5bc9614431b6ba0d
Dusty <dustymccord@gmail.com>
2020-02-19 11:09:04 -0800

    add listener for room view in the URL.

--- src/render.js
+++ src/render.js
@@ -1,19 +1,19 @@
 import $ from "jquery";
 
 export class Render {
+  constructor() {
+    this.listObj = [];
+  }
 
-    constructor() {
-        this.listObj = [];
-    }
-
-    ytSearch(results) {
-        let printString = "";
-        let {items} = results;
-               items.forEach((video, index)=>{
-            printString += `
+  ytSearch(results) {
+    let printString = "";
+    let { items } = results;
+    items.forEach((video, index) => {
+      printString += `
             <div class="ytSong"> 
                 <div class="ytSong-video">
-                    <iframe id="player" type="text/html" src="http://www.youtube.com/embed/${video.id.videoId}" frameborder="0"></iframe>
+                    <iframe id="player" type="text/html" src="http://www.youtube.com/embed/${video.id
+                      .videoId}" frameborder="0"></iframe>
                 </div>
             <div class="ytSong-info">
                 <h4 class="ytSong-title"> ${video.snippet.title} </h4>
@@ -22,44 +22,46 @@ export class Render {
                     <button id="${index}" class="module hvr-sweep-to-right hvr-icon-forward" type="button">Add Song   <i class="hvr-icon fas fa-arrow-right"></i> </button>
                 </div>
             </div>
-            </div>`
-        });
-        $(".search-results").html(printString);
-    }
+            </div>`;
+    });
+    $(".search-results").html(printString);
+  }
 
-    playlist(results) {
-        let printString = "";
-        this.clearPlayListObj();
-        results.forEach((item) => {
-            let { videoLink, order, user, createdAt, videoName } = item.data()
-            let videoItem = {
-                docId: item.id,
-                videoLink,
-                order,
-                user,
-                createdAt,
-                videoName
-            }
+  playlist(results) {
+    let printString = "";
+    this.clearPlayListObj();
+    results.forEach((item) => {
+      let { videoLink, order, user, createdAt, videoName } = item.data();
+      let videoItem = {
+        docId: item.id,
+        videoLink,
+        order,
+        user,
+        createdAt,
+        videoName
+      };
 
-            this.listObj.push(videoItem);
-            printString += `<div name="${item.data().videoLink}" id="${item.id}" class="playlist-box">
+      this.listObj.push(videoItem);
+      printString += `<div name="${item.data().videoLink}" id="${item.id}" class="playlist-box">
                 <div class ="playlist--title"> ${item.data().videoName} </div>
                 <div class="playlist--order"> ${item.data().order} <img src="${item.data().img}"></div>
                 <div class="playlist--user"> ${item.data().user} </div>
                 <div class="playlist--buttons">
                     <button class="btn btn-danger delete" name="${item.id}">delete song</button>
-                    <button class="btn btn-success moveUp" name="${item.id}" value="${item.data().order}"><i class="fas fa-arrow-up"></i></button>
-                    <button class="btn btn-success moveDown" name="${item.id}" value="${item.data().order}"><i class="fas fa-arrow-down"></i></button>
+                    <button class="btn btn-success moveUp" name="${item.id}" value="${item.data()
+        .order}"><i class="fas fa-arrow-up"></i></button>
+                    <button class="btn btn-success moveDown" name="${item.id}" value="${item.data()
+        .order}"><i class="fas fa-arrow-down"></i></button>
                 </div>
-            </div>`
-        });
-        $(".playlist-render").html(printString);
-    }
+            </div>`;
+    });
+    $(".playlist-render").html(printString);
+  }
 
-    roomList(rooms) {
-        let printString = "";
-        rooms.forEach((item) => {
-            printString += `<li>
+  roomList(rooms) {
+    let printString = "";
+    rooms.forEach((item) => {
+      printString += `<li>
                 <div>
                     ${item.data().roomName} 
                     <button class="show-playlist" value="${item.id}">show-playlist</button>
@@ -71,37 +73,12 @@ export class Render {
                     <input id="${item.id}-input" type="text" value="https://karaoke-team.web.app/?${item.id}"> 
                     <button name="${item.id}" class="copy-to-clipboard"> Copy Link </button>
                 </div>
-            </li>`
-        });
-        $(".rooms--list").html(printString);
-    }
+            </li>`;
+    });
+    $(".rooms--list").html(printString);
+  }
 
-    clearPlayListObj(){
-        this.listObj = [];
-    }
-
-    roomListListen() {
-        $('.rooms--list').on('click', '.copy-to-clipboard', function () {
-            let nameValue = this.name;
-            let copyInput = document.getElementById(`${nameValue}-input`);
-            copyInput.select();
-            copyInput.setSelectionRange(0, 99999);
-            document.execCommand("copy");
-        });
-
-        // $('.rooms--list').on('click', '.show-playlist', function () {
-        //     console.log('click');
-        // });
-        $('.rooms--list').on('click', '.show-main-show', function () {
-            console.log('click');
-        });
-        $('.rooms--list').on('click', '.show-invite', function () {
-            let valueOfButton = this.value;
-            $(`#share-link-${valueOfButton}`).slideToggle();
-        });
-    }
-
-        
-        
-        
+  clearPlayListObj() {
+    this.listObj = [];
+  }
 }
